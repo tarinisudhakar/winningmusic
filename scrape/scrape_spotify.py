9261f5674c927425675e380096bb9ec530cb24b6
Tarini Sudhakar <tarinisudhakar@Tarinis-MacBook-Air-2.local>
2023-04-18 13:10:05 -0500

    Scraping code

--- scrape/scrape_spotify.py
+++ scrape/scrape_spotify.py
@@ -1,1 +1,71 @@
+import spotipy 
+from spotipy.oauth2 import SpotifyClientCredentials
+import csv
+import os
+import pandas as pd
+import numpy as np 
+from collections import OrderedDict
+
+#Authentication - without user
+cid = "ENTER HERE"
+secret = "ENTER HERE"
+
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+#Import tracks from .csv
+bbcsv = pd.read_csv('billboard_top_songs.csv')
+
+df2 = bbcsv[['title', 'artist']].copy()
+
+df2.replace('\'','', regex=True, inplace=True)
+df2.replace('\"','', regex=True, inplace=True)
+#df2.replace('','', regex=True, inplace=True)
+#df2.replace('[^A-Za-z\s]','', regex=True, inplace=True)
+
+sample = df2.head(200)
+
+#Get Track ID for single case in case of errors
+artist = sample['artist'][22]
+track = sample['title'][22]
+track_id = sp.search(q='artist:' + artist + ' track:' + track, type='track', market = 'US')
+features_to_df = np.array(track_features)[0]
+features_to_df = OrderedDict(features_to_df)
+feature_values = features_to_df.values()
+
+
+#Function for all data
+def get_spotify_features(track, artist):
+    #Search for Spofity song ID 
+    songs=sp.search(q='track:'+track+' '+'artist:'+artist+'*' , type='track', market = 'US')
+    items = songs['tracks']['items']
+    if len(items) ==0:
+        return([0]*len(feature_values))
+    else:
+        track = items[0]
+        song_id = str(track["id"])
+        #Use ID to get Song features
+        track_features=sp.audio_features(song_id)
+        #if len(track_features[0]) <18:
+        #    return([0]*len(feature_values))
+        if len(track_features[0]) = None:
+            continue 
+        else:
+            features_to_df = np.array(track_features)[0]
+            #Order Dictionary
+            features_to_df = OrderedDict(features_to_df)
+            #Get Dictionary values
+            feature_values = features_to_df.values()
+            return(feature_values)
+
+dfx = df2.apply(lambda x: get_spotify_features(x['title'], x['artist']), axis=1)
+df1 = pd.DataFrame(dfx.tolist())
+df1.columns = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 
+'liveness', 'valence', 'tempo', 'type', 'id', 'uri', 'track_href', 'analysis_url', 'duration_ms', 'time_signature']
+
+#Merge dataframes 
+df2.join(df1)
+
+
+
 
